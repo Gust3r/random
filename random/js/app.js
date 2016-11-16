46fd91c7879187c4e05bf164e4646ab8fb586dec
Gustavo Ribeiro <gustavo.ottoni.ribeiro@gmail.com>
2016-11-15 18:40:52 -0700

    injected ui router into app.js. still need to bring in the cdn.

--- random/js/app.js
+++ random/js/app.js
@@ -1,1 +1,12 @@
-angular.module("myApp",[]);
+angular.module("myApp", ["ui.router"]).config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise("/");
+
+  $stateProvider
+  .state("home", {
+    url:"/",
+    controller: "homeCtrl",
+    templateUrl: "./views/home/homeTemplate.html"
+  });
+
+
+});
